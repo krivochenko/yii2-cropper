78b3b77d37711a2b8234f2f46f88252d985cfb26
krivochenko <budyaga@gmail.com>
2015-05-11 02:55:16 +0300

    [new] - merge Upload and Crop actions in one

--- actions/UploadAction.php
+++ actions/UploadAction.php
@@ -10,12 +10,13 @@ use yii\web\Response;
 use yii\web\UploadedFile;
 use budyaga\cropper\Widget;
 use yii\imagine\Image;
+use Imagine\Image\Box;
 use Yii;
 
 
 class UploadAction extends Action
 {
-    public $tmpPath;
+    public $path;
     public $url;
     public $uploadParam = 'file';
     public $maxSize = 2097152;
@@ -34,10 +35,10 @@ class UploadAction extends Action
         } else {
             $this->url = rtrim($this->url, '/') . '/';
         }
-        if ($this->tmpPath === null) {
-            throw new InvalidConfigException(Yii::t('cropper', 'MISSING_ATTRIBUTE', ['attribute' => 'tmpPath']));
+        if ($this->path === null) {
+            throw new InvalidConfigException(Yii::t('cropper', 'MISSING_ATTRIBUTE', ['attribute' => 'path']));
         } else {
-            $this->tmpPath = rtrim(Yii::getAlias($this->tmpPath), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+            $this->path = rtrim(Yii::getAlias($this->path), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
         }
     }
 
@@ -62,15 +63,20 @@ class UploadAction extends Action
                 ];
             } else {
                 $model->{$this->uploadParam}->name = uniqid() . '.' . $model->{$this->uploadParam}->extension;
+                $request = Yii::$app->request;
 
-                $image = Image::getImagine()->open($file->tempName);
+                $image = Image::crop(
+                    $file->tempName . $request->post('filename'),
+                    intval($request->post('w')),
+                    intval($request->post('h')),
+                    [$request->post('x'), $request->post('y')]
+                )->resize(
+                    new Box($this->width, $this->height)
+                );
 
-                if ($image->save($this->tmpPath . $model->{$this->uploadParam}->name)) {
+                if ($image->save($this->path . $model->{$this->uploadParam}->name)) {
                     $result = [
-                        'filelink' => $this->url . $model->{$this->uploadParam}->name,
-                        'filename' => $model->{$this->uploadParam}->name,
-                        'width' => $image->getSize()->getWidth(),
-                        'height' => $image->getSize()->getHeight()
+                        'filelink' => $this->url . $model->{$this->uploadParam}->name
                     ];
                 } else {
                     $result = [
