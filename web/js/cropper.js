40387c63b1c706b481f873ddc4b7a847a2b16f50
Sergey Krivochenko <budyaga@gmail.com>
2015-12-28 19:32:40 +0300

    [fix] - refactoring cropper.js

--- web/js/cropper.js
+++ web/js/cropper.js
@@ -1,137 +1,134 @@
 (function ($) {
     $.fn.cropper = function (options, width, height) {
-        var $widget = getWidget(this);
-        var $progress = $widget.find('.progress');
-        var buttons = [
-            $widget.find('.cropper_label'),
-            $widget.find('.upload_new_photo')
-        ];
+        var cropper = {
+            $widget: $(this).parent('.cropper_widget'),
+            $progress: $(this).parent('.cropper_widget').find('.progress'),
+            $thumbnail: $(this).find('.thumbnail'),
+            uploader: null,
+            reader: null,
+            selectedFile: null,
+            init: function() {
+                cropper.reader = new FileReader();
+                cropper.reader.onload = function(e) {
+                    cropper.clearOldImg();
 
-        var settings = $.extend({
-            button: buttons,
-            dropzone: $widget.find('.cropper_label'),
-            responseType: "json",
-            noParams: true,
-            multipart: true,
-            autoSubmit: false,
-            onChange: function (filename, extension, size) {
-                this.clearQueue();
+                    cropper.$widget.find('.new_photo_area').append('<img src="' + e.target.result + '">');
+                    cropper.$img = cropper.$widget.find('.new_photo_area img');
 
-
-                if (!inArray(options['allowedExtensions'], extension.toLowerCase())) {
-                    showError($widget, options['ext_error_text']);
-                    $progress.addClass('hidden');
-                    return false;
-                }
-
-                $progress.removeClass('hidden').find('.progress-bar').css({'width': '55%'});
-
-                if (size > options['maxSize']) {
-                    showError($widget, options['size_error_text']);
-                    $progress.addClass('hidden');
-                    return false;
-                }
-
-                if (!inArray(options['allowedExtensions'], extension)) {
-                    showError($widget, options['ext_error_text']);
-                    $progress.addClass('hidden');
-                    return false;
-                }
-
-                showError($widget, '');
-                $widget.find('.cropper_buttons').removeClass('hidden');
-                $widget.find('.cropper_label').addClass('hidden');
-
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    var $img = $widget.find('.new_photo_area img');
-                    if ($img.length) {
-                        $img.data('Jcrop').destroy();
-                        $img.remove();
-                    }
-
-                    $widget.find('.new_photo_area').append('<img src="' + e.target.result + '">');
-                    $img = $widget.find('.new_photo_area img');
-
-                    var x1 = ($img.width() - width) / 2;
-                    var y1 = ($img.height() - height) / 2;
+                    var x1 = (cropper.$img.width() - width) / 2;
+                    var y1 = (cropper.$img.height() - height) / 2;
                     var x2 = x1 + width;
                     var y2 = y1 + height;
 
-                    $img.Jcrop({
+                    cropper.$img.Jcrop({
                         aspectRatio: width / height,
                         setSelect: [x1, y1, x2, y2],
-                        boxWidth: $widget.find('.new_photo_area').width(),
-                        boxHeight: $widget.find('.new_photo_area').height()
+                        boxWidth: cropper.$widget.find('.new_photo_area').width(),
+                        boxHeight: cropper.$widget.find('.new_photo_area').height()
                     });
 
-                    $progress.addClass('hidden').find('.progress-bar').css({'width': '0%'});
+                    cropper.setProgress(0);
                 };
-                reader.readAsDataURL(this._input.files[0]);
+
+                var settings = $.extend({
+                    button: [
+                        cropper.$widget.find('.cropper_label'),
+                        cropper.$widget.find('.upload_new_photo')
+                    ],
+                    dropzone: cropper.$widget.find('.cropper_label'),
+                    responseType: 'json',
+                    noParams: true,
+                    multipart: true,
+                    onChange: function() {
+                        if (cropper.selectedFile) {
+                            cropper.selectedFile = null;
+                            cropper.uploader._queue = [];
+                        }
+                        return true;
+                    },
+                    onSubmit: function() {
+                        if (cropper.selectedFile) {
+                            return true;
+                        }
+                        cropper.selectedFile = cropper.uploader._queue[0];
+
+                        cropper.setProgress(55);
+                        cropper.showError('');
+                        cropper.reader.readAsDataURL(this._queue[0].file);
+                        return false;
+                    },
+                    onComplete: function(filename, response) {
+                        cropper.$progress.addClass('hidden');
+                        if (response['error']) {
+                            cropper.showError(response['error']);
+                            return;
+                        }
+                        cropper.showError('');
+
+                        cropper.$widget.find('.thumbnail').attr({'src': response['filelink']});
+                        cropper.$widget.find('.photo_field').val(response['filelink']);
+                    },
+                    onSizeError: function () {
+                        cropper.showError(options['size_error_text']);
+                        cropper.cropper.setProgress(0);
+                    },
+                    onExtError: function () {
+                        cropper.showError(options['ext_error_text']);
+                        cropper.setProgress(0);
+                    }
+                }, options);
+
+                cropper.uploader = new ss.SimpleUpload(settings);
+
+                cropper.$widget.on('click', '.delete_photo', function() {
+                    cropper.deletePhoto();
+                }).on('click', '.crop_photo', function() {
+                    var data = cropper.$img.data('Jcrop').tellSelect();
+                    data[yii.getCsrfParam()] = yii.getCsrfToken();
+
+                    if (cropper.uploader._queue.length) {
+                        cropper.selectedFile = cropper.uploader._queue[0];
+                    } else {
+                        cropper.uploader._queue[0] = cropper.selectedFile;
+                    }
+                    cropper.uploader.setData(data);
+
+                    cropper.setProgress(1);
+                    cropper.uploader.setProgressBar(cropper.$progress.find('.progress-bar'));
+
+                    cropper.readyForSubmit = true;
+                    cropper.uploader.submit();
+                });
             },
-            onComplete: function (filename, response) {
-                $progress.addClass('hidden');
-                if (response.error) {
-                    showError($widget, response.error);
-                    return;
+            showError: function(error) {
+                if (error == '') {
+                    cropper.$widget.parents('.form-group').removeClass('has-error').find('.help-block').text('');
+                } else {
+                    cropper.$widget.parents('.form-group').addClass('has-error').find('.help-block').text(error);
                 }
-                showError($widget, '');
-
-                $widget.find('.thumbnail').attr({'src': response.filelink});
-                $widget.find('.photo_field').val(response.filelink);
             },
-            onSizeError: function () {
-                showError($widget, options['size_error_text'])
+            setProgress: function(value) {
+                if (value) {
+                    cropper.$widget.find('.cropper_buttons').removeClass('hidden');
+                    cropper.$widget.find('.cropper_label').addClass('hidden');
+                    cropper.$progress.removeClass('hidden').find('.progress-bar').css({'width': value + '%'});
+                } else {
+                    cropper.$progress.addClass('hidden').find('.progress-bar').css({'width': 0});
+                }
+            },
+            deletePhoto: function() {
+                cropper.$widget.find('.photo_field').val('');
+                cropper.$thumbnail.attr({'src': cropper.$thumbnail.data('no-photo')});
+            },
+            clearOldImg: function() {
+                if (cropper.$img) {
+                    cropper.$img.data('Jcrop').destroy();
+                    cropper.$img.remove();
+                    cropper.$img = null;
+                }
             }
-        }, options);
+        };
 
-        var $uploader = new ss.SimpleUpload(settings);
-        $widget.data('uploader', $uploader);
-        $widget.data('progress', $progress);
+        cropper.init();
     };
-
-    $('.cropper_widget').on('click', '.delete_photo', function () {
-        var $widget = getWidget($(this));
-        var $thumbnail = $widget.find('.thumbnail');
-        $widget.find('.photo_field').val('');
-        $thumbnail.attr({'src': $thumbnail.data('no-photo')});
-    }).on('click', '.crop_photo', function () {
-        var $widget = getWidget($(this));
-        var $img = $widget.find('.new_photo_area img');
-        var data = $img.data('Jcrop').tellSelect();
-        data[yii.getCsrfParam()] = yii.getCsrfToken();
-
-        var $uploader = $widget.data('uploader');
-
-        $uploader._queue[1] = $uploader._queue[0];
-        $uploader.setData(data);
-
-        var $progress = $widget.find('.progress');
-        $progress.removeClass('hidden');
-        $uploader.setProgressBar($progress.find('.progress-bar'));
-
-        $uploader.submit();
-    });
-
-    function getWidget($element) {
-        return $element.parents('.cropper_widget');
-    }
-
-    function showError($widget, error) {
-        if (error == '') {
-            $widget.parents('.form-group').removeClass('has-error').find('.help-block').text('');
-        } else {
-            $widget.parents('.form-group').addClass('has-error').find('.help-block').text(error);
-        }
-    }
-
-    function inArray(haystack, needle) {
-        var length = haystack.length;
-        for (var i = 0; i < length; i++) {
-            if (haystack[i] == needle) {
-                return true;
-            }
-        }
-        return false;
-    }
 })(jQuery);
