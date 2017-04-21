a329bf8ba52860b60032aca7dd104e62f4c05c90
nyatw <nyatwo.exe@gmail.com>
2017-04-21 15:04:01 +0500

    Update UploadAction.php

--- actions/UploadAction.php
+++ actions/UploadAction.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace budyaga\cropper\actions;
+namespace nyatw\cropper\actions;
 
 use yii\base\Action;
 use yii\base\DynamicModel;
@@ -8,7 +8,7 @@ use yii\base\InvalidConfigException;
 use yii\web\BadRequestHttpException;
 use yii\web\Response;
 use yii\web\UploadedFile;
-use budyaga\cropper\Widget;
+use nyatw\cropper\Widget;
 use yii\imagine\Image;
 use Imagine\Image\Box;
 use Yii;
