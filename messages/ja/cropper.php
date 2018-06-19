94fd25e3c14bbfc0ba6ff7cccd32df14bba9bb8e
Nobuo Kihara <softark@gmail.com>
2018-06-19 13:49:36 +0900

    messages/ja/cropper.php revised

--- messages/ja/cropper.php
+++ messages/ja/cropper.php
@@ -21,9 +21,9 @@ return [
     'CROP_PHOTO' => '画像を切り抜き',
     'DELETE_PHOTO' => '画像を消去',
     'ERROR_CAN_NOT_UPLOAD_FILE' => 'ファイルをアップロード出来ません',
-    'MISSING_ATTRIBUTE' => '"{attribute}" を指定しなければいけません',
+    'MISSING_ATTRIBUTE' => '"{attribute}" は必須です',
     'ONLY_POST_REQUEST' => 'POST リクエストのみ許可されています',
-    'UPLOAD_ANOTHER_PHOTO' => '別の写真をアップロード',
+    'UPLOAD_ANOTHER_PHOTO' => '別の画像をアップロード',
     'TOO_BIG_ERROR' => '許可されているファイル・サイズ({size} Mb)を超過しています',
     'EXTENSION_ERROR' => '以下のファイル形式だけが許可されています: {formats}'
 ];
