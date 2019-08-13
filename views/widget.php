faf3c580061b04b27ea060e8a2b9008dc1f04c2b
viper_work <wiperawa@gmail.com>
2019-08-13 16:57:17 +0500

    Fixed layout a little bit to to work with flexbox. added .hidden class with !important derictive, because in Yii2 with Bs4 after uploading 'copper-label' div didnt hide.

--- views/widget.php
+++ views/widget.php
@@ -9,10 +9,11 @@ use yii\helpers\Html;
 
 ?>
 
-<div class="cropper-widget">
+<div class="cropper-widget" style="display: flex; flex: 0 0 auto;align-items: center;flex-direction: column;">
     <?= Html::activeHiddenInput($model, $widget->attribute, ['class' => 'photo-field']); ?>
     <?= Html::hiddenInput('width', $widget->width, ['class' => 'width-input']); ?>
     <?= Html::hiddenInput('height', $widget->height, ['class' => 'height-input']); ?>
+    <div class="row cropper-row">
     <?= Html::img(
         $model->{$widget->attribute} != ''
             ? $model->{$widget->attribute}
@@ -23,7 +24,20 @@ use yii\helpers\Html;
             'data-no-photo' => $widget->noPhotoImage
         ]
     ); ?>
+    <div class="new-photo-area" style="height: <?= $widget->cropAreaHeight; ?>px; width: <?= $widget->cropAreaWidth; ?>px;">
+        <div class="cropper-label">
+            <span><?= $widget->label;?></span>
+        </div>
+            <div class="progress hidden" style="width: <?= $widget->cropAreaWidth; ?>px;">
+        <div class="progress-bar progress-bar-striped progress-bar-success active" role="progressbar" style="width: 0%">
+            <span class="sr-only"></span>
+        </div>
+    </div>
 
+    </div>
+
+    </div>
+    <div class="row cropper-row">
     <div class="cropper-buttons">
         <button type="button" class="btn btn-sm btn-danger delete-photo" aria-label="<?= Yii::t('cropper', 'DELETE_PHOTO');?>">
             <span class="glyphicon glyphicon-trash" aria-hidden="true"></span> <?= Yii::t('cropper', 'DELETE_PHOTO');?>
@@ -35,15 +49,20 @@ use yii\helpers\Html;
             <span class="glyphicon glyphicon-picture" aria-hidden="true"></span> <?= Yii::t('cropper', 'UPLOAD_ANOTHER_PHOTO');?>
         </button>
     </div>
-
-    <div class="new-photo-area" style="height: <?= $widget->cropAreaHeight; ?>px; width: <?= $widget->cropAreaWidth; ?>px;">
-        <div class="cropper-label">
-            <span><?= $widget->label;?></span>
-        </div>
-    </div>
-    <div class="progress hidden" style="width: <?= $widget->cropAreaWidth; ?>px;">
-        <div class="progress-bar progress-bar-striped progress-bar-success active" role="progressbar" style="width: 0%">
-            <span class="sr-only"></span>
-        </div>
     </div>
 </div>
+
+<?php
+$css = <<<CSS
+.hidden {
+    display: none !important;
+}
+.cropper-row {
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+}
+CSS;
+
+$this->registerCss($css);
+?>
